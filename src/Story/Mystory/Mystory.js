7b15d8ea695b51a9bbce2ad5ddda9eb461921f26
Naseerj <jenrolaolasupo@gmail.com>
2022-12-19 13:46:08 +0100

    New Page

--- src/Story/Mystory/Mystory.js
+++ src/Story/Mystory/Mystory.js
@@ -1,12 +1,23 @@
-import React from 'react'
-import './Mystory.css'
+import React from "react";
+import "./Mystory.css";
 
 const Mystory = () => {
   return (
     <div>
-        mystory
+      <div className="d-firstdiv">
+        <h1>My story</h1>
+        <button>Write Story</button>
+      </div>
+      <div className="d-parentdiv">
+        <div className="d-secondiv">
+          <a href="">All</a>
+          <a href="">Drafts</a>
+          <a href="">Published</a>
+        </div>
+        <hr />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Mystory
+export default Mystory;
